32ce24926a1364a2aaf4b2af05cd97bcc404595c
Hecatonquir <maxicuentas92@hotmail.com>
2022-07-15 06:33:57 -0300

    changed the buda archive to have force:false, so it wont restart my db when this app  is deployed

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ const { conn } = require('./src/db.js');
 const { loadAllCountries } = require('./src/functions');
 
 // Syncing all the models at once.false
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
 	server.listen(process.env.PORT, async () => {
 		console.log(`%s listening at ${process.env.PORT}`);
 		await loadAllCountries();
