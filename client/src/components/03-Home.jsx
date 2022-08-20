33f5df0149cd35e061adfb2cc8f2b9615de8687a
Hecatonquir <maxicuentas92@hotmail.com>
2022-08-20 10:36:32 -0300

    improoved loading logo

--- client/src/components/03-Home.jsx
+++ client/src/components/03-Home.jsx
@@ -1,35 +1,38 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import "./Styles/03-Home.css";
-import * as A from "../Redux/actions";
-import * as Filter from "./08-Filters";
-import SearchBar from "./05-Searchbar";
-import Paginado from "./06-Paginado";
-import MainContainer from "./14-MainContainer";
-import Navbar from "./04-NavBar";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import * as A from '../Redux/actions';
+import * as Filter from './08-Filters';
+import SearchBar from './05-Searchbar';
+import Paginado from './06-Paginado';
+import MainContainer from './14-MainContainer';
+import Navbar from './04-NavBar';
+
+import './Styles/03-Home.css';
 
 export default function Home() {
 	const dispatch = useDispatch();
-	let countriesState = useSelector((state) => state.countries);
+	let countriesState = 0; //useSelector((state) => state.countries);
 	countriesState?.length ? (
-		typeof countriesState != "string" ? (
+		typeof countriesState != 'string' ? (
 			countriesState.map((c) => {
 				let a = c;
 				a.Nombre = c.Nombre.replace(/(^\w{1})|(\s+\w{1})/g, (letter) => letter.toUpperCase());
 				return a;
 			})
 		) : (
-			<h2 className="notFound">{countriesState}</h2>
+			<h2 className='notFound'>{countriesState}</h2>
 		)
 	) : (
-		<div className="loading">
+		<div className='loading'>
 			<img
-				className="loading"
-				src="https://cdn.dribbble.com/users/1731126/screenshots/4339345/hipster-pilot-v2.gif"
-				alt="Uups..."
+				className='loading'
+				src='https://cdn.dribbble.com/users/1731126/screenshots/4339345/hipster-pilot-v2.gif'
+				alt='Uups...'
 			/>
-			<h2 className="loading"> Loading...</h2>
+			<h2 className='loading' >
+				Loading...
+			</h2>
 		</div>
 	);
 
@@ -37,18 +40,18 @@ export default function Home() {
 	const [currentPage, setCurrentPage] = useState(1);
 	let countriesPerPage = 10;
 	// eslint-disable-next-line no-unused-vars
-	const [orden, setOrden] = useState("");
+	const [orden, setOrden] = useState('');
 	const indexOfLastCountry = currentPage * countriesPerPage;
 	const indexOfFirstCountry = indexOfLastCountry - countriesPerPage;
 	let currentcountries;
 	if (currentPage === 1) {
 		currentcountries = countriesState?.length
 			? countriesState.slice(indexOfFirstCountry, indexOfLastCountry - 1)
-			: "No hay countries";
+			: 'No hay countries';
 	} else
 		currentcountries = countriesState?.length
 			? countriesState.slice(indexOfFirstCountry - 1, indexOfLastCountry - 1)
-			: "No hay countries";
+			: 'No hay countries';
 	// __________________________ Fin Paginado _____________________________
 	useEffect(() => {
 		dispatch(A.getAllCountries());
@@ -56,11 +59,11 @@ export default function Home() {
 	}, [dispatch]);
 
 	return (
-		<main className="home">
+		<main className='home'>
 			<Navbar />
 			<SearchBar setCurrentPage={setCurrentPage} />
 			<br />
-			<div className="filtros">
+			<div className='filtros'>
 				<Filter.ByContinent setCurrentPage={setCurrentPage} />
 				<Filter.ByActivity setCurrentPage={setCurrentPage} />
 				<Filter.Alphabetically setCurrentPage={setCurrentPage} setOrden={setOrden} />
